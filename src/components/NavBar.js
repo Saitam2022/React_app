b8ff2a6a024793bed89279303333c1231ae291a5
Angel <ortizstark.m@gmail.com>
2022-07-27 19:53:42 -0300

    Desafio Menu e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+export default function NavBar() {
+
+    return <nav className="nav">
+        <a href="/" className="site-title">Drummer E-commerce</a>
+        <ul>
+            <li>
+                <a href="/baterias nuevas">Baterias Nuevas</a>
+            </li>
+            <li>
+                <a href="/baterias usadas">Baterias Usadas</a>
+            </li>
+            <li>
+                <a href="/sobre nosotros">Sobre Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+}
